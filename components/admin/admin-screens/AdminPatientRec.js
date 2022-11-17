63ed4f0789c8391bbcbd2bf7a01e4d467f6b39f0
Mary Angel <201910994@fit.edu.ph>
2022-11-18 01:43:57 +0800

    section list

--- components/admin/admin-screens/AdminPatientRec.js
+++ components/admin/admin-screens/AdminPatientRec.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, ImageBackground, FlatList, View, TextInput, Pressable, Alert, Text, StatusBar, Image } from 'react-native';
+import { StyleSheet, ImageBackground, FlatList, View, TextInput, Pressable, Alert, Text, StatusBar, Image, Modal, SectionList } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import {
   responsiveHeight,
@@ -13,7 +14,7 @@ const showAlert = () =>
     "Search Button Working",
   );
 
-export default function AdminPatientRec() {
+export default function AdminPatientRec({navigation}) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [data2, setData2] = useState([]);
@@ -31,7 +32,7 @@ export default function AdminPatientRec() {
     var patientrec = "http://192.168.1.5:80/epmc-4/api/Admin_dashboard/total";
     var patientrec2 = "http://192.168.2.115:80/epmc-4/adm_patientrec_total";
   
-    await fetch(patientrec2,{
+    await fetch(patientrec,{
       headers: headers
     })  
     .then((response)=>response.json())
@@ -51,7 +52,7 @@ export default function AdminPatientRec() {
     var patientrec = "http://192.168.1.5:80/epmc-4/adm_patientrec_patients";
     var patientrec2 = "http://192.168.2.115:80/epmc-4/adm_patientrec_patients";
   
-    await fetch(patientrec2,{
+    await fetch(patientrec,{
       headers: headers
     })  
     .then((response)=>response.json())
@@ -73,9 +74,86 @@ export default function AdminPatientRec() {
   const itemSeparator = () => {
     return <View style={styles.separator}/>
   }
-  
+
+  //Section List - Patient Record View
+  const DATA = [
+    {
+      title: "Personal Information",
+      data: [{id: "avatar", value : "image"}, {id: "id", value : "PMC"}, {id: "3", value : "Risotto"}]
+    },
+    {
+      title: "Contact Information",
+      data: [{id: "avatar", value : "image"}, {id: "id", value : "PMC"}, {id: "3", value : "Risotto"}]
+    },
+    {
+      title: "Emergency Information",
+      data: [{id: "avatar", value : "image"}, {id: "id", value : "PMC"}, {id: "3", value : "Risotto"}]
+    },
+    {
+      title: "Vital Signs",
+      data: [{id: "avatar", value : "image"}, {id: "id", value : "PMC"}, {id: "3", value : "Risotto"}]
+    },
+  ];
+
+  // function ListItem({
+  //   navigation,
+  //   section,
+  //   item,
+  //   index,
+  // })
+
+
+
+  // const Item = ({ title }) => (
+  //   <View style={styles.recordContainer}>
+  //     <View style={styles.recordDataContainer}>
+  //       {/* <Text>hello</Text> */}
+  //       <Text style={styles.recordData}>hello {title}</Text>
+  //     </View>
+  //   </View>
+    
+  // );
+  //End Section List - Patient Record View
+
+  // Modal for Patient Record
+  const [modal1Visible, setmodal1Visible] = useState(false);
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={modal1Visible}
+        onRequestClose={() => {
+          setmodal1Visible(!modal1Visible);
+        }} >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView} >
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setmodal1Visible(!modal1Visible)} >
+                  <AntDesign name='closecircle' size={20} style={styles.closeBtn} />
+              </Pressable>
+              {/* SECTION LIST */}
+              <SectionList
+                sections={DATA}
+                // keyExtractor={(item, index) => item + index}
+                renderItem={({ item, index }) => 
+                  <View style={styles.recordContainer}>
+                      <View style={styles.recordDataContainer}>
+                        <Text style={styles.recordData}>{item.id}: {item.value} </Text>
+                      </View>
+                  </View>
+                  
+                }
+                renderSectionHeader={({ section: { title } }) => (
+                  <Text style={styles.recordheader}>{title}</Text>
+                )}
+              />
+            </View>
+          </View>
+      </Modal>
+
       {isLoading ? <Text style={styles.loadingtext}>Loading Data...</Text>:
       <ImageBackground source={require('../../../assets/patientrecbg.png')} style={styles.bgimage}>
         <View style={styles.searchBar}>
@@ -92,24 +170,26 @@ export default function AdminPatientRec() {
 
         <View style={styles.patientlist}>
           <FlatList
-          ListHeaderComponentStyle={styles.listHeader}
-          ListHeaderComponent={headerComponent}
-          data={data2}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <View style={styles.avatarContainer}>
-              <Image source={{uri:item.image}} style={styles.avatar}/>
+            ListHeaderComponentStyle={styles.listHeader}
+            ListHeaderComponent={headerComponent}
+            data={data2}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <View style={styles.avatarContainer}>
+                  <Image source={{uri:item.image}} style={styles.avatar}/>
+                </View>
+                <Text style={styles.nametxt}>{item.pt_fullname}</Text>
+
+                <View style={styles.viewContainer}>
+                  <Pressable onPress={() => setmodal1Visible(true)} style={styles.viewbox}>
+                    <Text style={styles.viewtxt}>View</Text>
+                  </Pressable>
+                </View>
+
               </View>
-              <Text>{item.pt_fullname}</Text>
-              <View style={styles.logoutContainer}>
-              <Pressable style={styles.logoutbox}>
-                <Text style={styles.viewtxt}>View</Text>
-              </Pressable>
-              </View>
-            </View>
-          )}
-          ItemSeparatorComponent = {itemSeparator}
+            )}
+            ItemSeparatorComponent = {itemSeparator}
           />
         </View>
         
@@ -173,6 +253,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginVertical: responsiveHeight(1.5),
     marginHorizontal: responsiveWidth(10),
+    padding: 20,
     width: responsiveWidth(80),
     height: responsiveHeight(70),
     borderRadius: 20,
@@ -200,6 +281,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     paddingVertical: 13,
   },
 
@@ -219,28 +301,107 @@ const styles = StyleSheet.create({
     width: 75,
   },
 
-  logoutContainer: {
-    height: 90,
-    width: 90,
-    marginLeft: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+  nametxt: {
+    flex: 1,
+    textAlign: 'left',
   },
 
-  logoutbox: {
-    alignItems: 'center',
+  viewContainer: {
+    height: 90,
+    width: 90,
+    marginRight: 10,
+    // marginLeft: 10,
+    // flexDirection: 'column',
     justifyContent: 'center',
-    backgroundColor: '#49bccf',
-    width: responsiveWidth(21),
+    alignItems: 'flex-end',
+    // alignSelf: 'flex-end',
+    
+  },
+
+  viewbox: {
+    alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#00DA9D',
+    width: responsiveWidth(15),
     padding: responsiveWidth(1.5),
     marginLeft: 5,
     borderRadius: 15,
     // elevation: 15,
     // borderWidth: 1,
-},
+  },
 
-logouttxt: {
-    fontSize: 16,
-    color: '#fff',
-},
+  logouttxt: {
+      fontSize: 16,
+      color: '#fff',
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: responsiveHeight(10),
+  },
+
+  modalView: {
+    width: responsiveWidth(90),
+    height: responsiveHeight(85),
+    marginHorizontal: responsiveWidth(5),
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    padding: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+
+  buttonClose: {
+    marginLeft: responsiveWidth(78),
+    marginTop: responsiveHeight(-1.5),
+  },
+
+  modalText: {
+    fontSize: responsiveFontSize(1.5),
+  },
+
+  //SECTION LIST STYLE
+  recordheader: {
+    backgroundColor: '#49bccf',
+    flex: 1,
+    textAlign: 'left',
+    width: responsiveWidth(80),
+    marginTop: responsiveHeight(1),
+    padding: 10,
+    paddingLeft: 0,
+    fontSize: responsiveFontSize(2),
+    borderWidth: 1,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+
+  recordContainer: {
+    flex: 1,
+    backgroundColor: '#49bccf',
+    // paddingTop: responsiveHeight(1),
+    // elevation: 5,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+
+  recordDataContainer: {
+    // backgroundColor: 'blue'
+    marginLeft: responsiveWidth(5),
+  },
+
+  recordData: {
+    // backgroundColor: 'blue'
+    fontSize: responsiveFontSize(1.5),
+  },
 });

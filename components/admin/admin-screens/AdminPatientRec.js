507b33b402f601201d4566b27f2cce728c51ac2c
Paul Jeremy Manuel <201910430@fit.edu.ph>
2022-11-17 19:36:37 +0800

    Patient rec

--- components/admin/admin-screens/AdminPatientRec.js
+++ components/admin/admin-screens/AdminPatientRec.js
@@ -1,4 +1,5 @@
-import { StyleSheet, ImageBackground, View, TextInput, Pressable, Alert, Text, StatusBar } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, ImageBackground, FlatList, View, TextInput, Pressable, Alert, Text, StatusBar, Image } from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import {
   responsiveHeight,
@@ -13,9 +14,69 @@ const showAlert = () =>
   );
 
 export default function AdminPatientRec() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type' : 'application/json;charset=UTF-8',
+    'X-API-KEY':'myapi',
+    'Authorization':'Basic YWRtaW46YWRtaW4xMjM='   
+  }
+
+  // fetching for Total
+  const fetchTotal = async () => {
+
+    var patientrec = "http://192.168.1.5:80/epmc-4/api/Admin_dashboard/total";
+    var patientrec2 = "http://192.168.2.115:80/epmc-4/adm_patientrec_total";
+  
+    await fetch(patientrec2,{
+      headers: headers
+    })  
+    .then((response)=>response.json())
+    .then((json)=>setData(json))
+    .catch((error)=> console.error("ERROR FOUND " + error))
+    .finally(() => setLoading(false));
+  }
+
+  useEffect(()=>{
+    fetchTotal();
+    const dataInterval = setInterval(() => fetchTotal(), 5 * 1000);
+    return () => clearInterval(dataInterval);
+  },[]);
+
+  const fetchPatient = async () => {
+
+    var patientrec = "http://192.168.1.5:80/epmc-4/api/Admin_patientrec_mobile/patients";
+    var patientrec2 = "http://192.168.2.115:80/epmc-4/adm_patientrec_patients";
+  
+    await fetch(patientrec2,{
+      headers: headers
+    })  
+    .then((response)=>response.json())
+    .then((json)=>setData2(json))
+    .catch((error)=> console.error("ERROR FOUND " + error))
+    .finally(() => setLoading(false));
+  }
+
+  useEffect(()=>{
+    fetchPatient();
+    const dataInterval = setInterval(() => fetchPatient(), 5 * 1000);
+    return () => clearInterval(dataInterval);
+  },[]);
+  
+  const headerComponent = () => {
+    return <Text style={styles.listHeadline}>Patient Record</Text>
+  }
+
+  const itemSeparator = () => {
+    return <View style={styles.separator}/>
+  }
   
   return (
     <View style={styles.container}>
+      {isLoading ? <Text style={styles.loadingtext}>Loading Data...</Text>:
       <ImageBackground source={require('../../../assets/patientrecbg.png')} style={styles.bgimage}>
         <View style={styles.searchBar}>
           <TextInput 
@@ -26,13 +87,34 @@ export default function AdminPatientRec() {
         </View>
 
         <View style={styles.total}>
-          <Text style={styles.txtTotal}>1702 Patients</Text>
+        {data.map(_total=><Text style={styles.txtTotal} key={_total.id}>{_total.patient} Patients</Text>)}
         </View>
 
         <View style={styles.patientlist}>
+          <FlatList
+          ListHeaderComponentStyle={styles.listHeader}
+          ListHeaderComponent={headerComponent}
+          data={data2}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <View style={styles.avatarContainer}>
+              <Image source={{uri:item.image}} style={styles.avatar}/>
+              </View>
+              <Text>{item.pt_fullname}</Text>
+              <View style={styles.logoutContainer}>
+              <Pressable style={styles.logoutbox}>
+                <Text style={styles.viewtxt}>View</Text>
+              </Pressable>
+              </View>
+            </View>
+          )}
+          ItemSeparatorComponent = {itemSeparator}
+          />
         </View>
         
       </ImageBackground>
+      }
     </View>
   );
 }
@@ -95,4 +177,70 @@ const styles = StyleSheet.create({
     height: responsiveHeight(70),
     borderRadius: 20,
   },
+
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor:'#CCC',
+  },
+
+  listHeader: {
+    height: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  listHeadline: {
+    color: '#333',
+    fontSize: 21,
+    fontWeight: 'bold'
+  },
+
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 13,
+  },
+
+  avatarContainer: {
+    // backgroundColor: '#D9D9D9',
+    // borderRadius: 100,
+    height: 90,
+    width: 90,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  avatar: {
+    borderRadius: 100,
+    height: 75,
+    width: 75,
+  },
+
+  logoutContainer: {
+    height: 90,
+    width: 90,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  logoutbox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#49bccf',
+    width: responsiveWidth(21),
+    padding: responsiveWidth(1.5),
+    marginLeft: 5,
+    borderRadius: 15,
+    // elevation: 15,
+    // borderWidth: 1,
+},
+
+logouttxt: {
+    fontSize: 16,
+    color: '#fff',
+},
 });
